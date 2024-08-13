4d188af37cb08e5a303d0eebd717106fde3a28ef
ItzARubix <83780010+ItzARubix@users.noreply.github.com>
2024-08-12 23:56:31 -0400

    Code

--- interpreter.java
+++ interpreter.java
@@ -2,7 +2,6 @@ import java.util.Stack;
 
 public class intepreter {
 	public static void main(String[] args) {
-		Stack tokens = new Stack();
-	}
-	
+		Stack<Integer> tokens = new Stack<Integer>();
+	}	
 }

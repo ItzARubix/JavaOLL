5df1b7f6785e5b953b684449b7f0d66a1c8abd35
ItzARubix <83780010+ItzARubix@users.noreply.github.com>
2024-08-20 16:14:16 -0400

    Added bash script to automate interpretation process

--- interpreter.java
+++ interpreter.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 import java.util.ArrayList;
 import java.io.IOException;
 
-public class intepreter {
+public class interpreter {
 	public static void main(String[] args) throws IOException {
 	
 		try {
